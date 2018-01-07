e81452ab14b05942d7a2c7424696db4d6fe6cf9c
valeriavalles <valeria.sv@outlook.es>
2018-01-06 22:46:27 -0500

    subiendo carpetas

---
+++ js/app.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+	setTimeout(function() {
+		window.location.href = 'views/contenido.html';
+    }, 4000);
+
+    setTimeout(function() {
+        $('.fly-in-text').removeClass('oculto')
+      
+    },700 );
+      
+});
+
+
---
+++ js/data.js
@@ -0,0 +1,46 @@
+var data = {
+  'comida mexicana': {
+    platos: [
+      { tacos:
+        {
+          src: '../assets/images/ceviche.jpg',
+          precio: '$20',
+        },  
+      },
+      { pescado:
+        {
+          src: '../assets/images/cubana.jpg',
+          precio: '$30',
+        },  
+      },
+      { wey:
+        {
+          src: '../assets/images/china.jpg',
+          precio: '$40',
+        },  
+      },
+    ]     
+  },
+  'comida peruana': {
+    platos: [
+      { pato:
+        {
+          src: '../assets/images/uno.jpg',
+          precio: '$20',
+        },  
+      },
+      { caucau:
+        {
+          src: '../assets/images/dos.jpg',
+          precio: '$22',
+        },  
+      },
+      { picante:
+        {
+          src: '../assets/images/tres.jpg',
+          precio: '$50',
+        },  
+      },
+    ]     
+  },
+};
