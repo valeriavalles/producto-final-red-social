d021e739515ef370a24b3100a1cd7a166d9bcda0
valeriavalles <valeria.sv@outlook.es>
2018-01-06 23:14:00 -0500

    actualizando archivos

--- js/app.js
+++ js/app.js
@@ -1,13 +1,3 @@
-$(document).ready(function() {
-	setTimeout(function() {
-		window.location.href = 'views/contenido.html';
-    }, 4000);
 
-    setTimeout(function() {
-        $('.fly-in-text').removeClass('oculto')
-      
-    },700 );
-      
-});
 
 
