f95bd50d2efc6d905ab829e81a58a372f5e0f5c5
valeriavalles <valeria.sv@outlook.es>
2018-01-12 10:03:19 -0500

    actulizando cambios para el posteo y subir fotos

--- js/contenido.js
+++ js/contenido.js
@@ -1,5 +1,19 @@
 /* ------------ FUNCIONALIDAD PARA POSTEAR(En proceso de mejora) ---------*/
 $(document).ready(function() {
+  /* ------ FUNCIONALIDAD PARA SUBIR FOTOS -------*/
+  var $board = $('#container-perfil');
+  var $btnImage = $('#photobtn');
+
+  $btnImage.on('click', function() {
+    var photo = $('#urlInput').val();
+    /** Vaciando los input. */
+    $('#urlInput').val('');
+    /** Contenedor donde irán los nuevos comentarios. */
+    var contenedor = $('#container-perfil');
+    
+    contenedor.append('<div class=box><div> <img class="photobox center-block img-responsive" src= ' + photo + '  alt=...> </div></div>');
+  });
+  /* ----------------- CODIGO PARA EL POSTEO... ----------*/
   $('#btn-publicar').attr('disabled', true);
   $('#textarea-publicar').keyup(function() {
     $('#btn-publicar').attr('disabled', false);
@@ -9,12 +23,30 @@ $(document).ready(function() {
 
   $('#btn-publicar').click(function() {
     if ($('#textarea-publicar').val().length > 0) {
-      var storyDiv = $('<div>', {'class': 'stylo-cont' });
-      var container = $('<p>');
+      // var storyDiv = $('<div>', {'class': 'stylo-cont' });
+      // var container = $('<p>');
       var text = $('#textarea-publicar').val();
-      storyDiv.text(text);
-	  storyDiv.append(container);
-	  $('#content-post').append(storyDiv);
+      // storyDiv.text(text);
+	  // storyDiv.append(container);
+      // $('#content-post').append(storyDiv);
+
+      // ======== Para no crear una estructura nueva se podria clonar la estructuras que ya tienes y cada ves que se publique solo se reemplace======
+
+      
+      // clonar estructura
+      // capturamos a la estructura a clonar
+      var $postclonado = $('#post-clonar').clone();
+      $postclonado.removeAttr('id');
+      $postclonado.attr('id', 'postnew');
+      $contentPost = $postclonado.find('#content-post').removeAttr('id');
+      $contentPost.attr('id', 'content-post-new');
+      $contentPost.text(text);
+      $imgUserPostClonado = $postclonado.find('#user').removeAttr('id');
+      $imgUserPostClonado.attr('id', 'user-new');
+      $imgUserPostClonado.attr('src', '../assets/images/user.png');
+      $nameUserPostClonado = $postclonado.find('#name-user').text('Alejandra Hoces');
+
+      $('#content-post').prepend($postclonado);
       $('#textarea-publicar').val('');
       $('#btn-publicar').attr('disabled', true);
       $('#textarea-publicar').css('initial');
@@ -22,7 +54,8 @@ $(document).ready(function() {
       $('#btn-publicar').attr('disabled', true);
     }
   });
-});
+});		  
+
 
 /* -----------  Fin FUNCIONALIDAD PARA POSTEAR---------*/
 /* ------------ FUNCIONALIDAD PARA SUBIR FOTOS ------------*/
